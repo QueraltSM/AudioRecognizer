26a86f0a263554d9e5c672bf78137a34a0e73ce6
QueraltSosaMompel <queraltsosa@hotmail.com>
2018-05-06 17:08:06 +0100

    Initial Commit

---
+++ AudioRecognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioRecognizer
+//
+//  Created by Queralt Sosa Mompel on 6/5/18.
+//  Copyright © 2018 Queralt Sosa Mompel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
